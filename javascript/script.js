dae228389baee5089bd0e84dfcaca7cd2c330f8e
RikkRenner <Rikk62@gmail.com>
2021-09-29 16:37:36 +0100

    exercises

--- javascript/script.js
+++ javascript/script.js
@@ -1,48 +1,62 @@
-// Console log
-console.log("Hello World!");
+// // FUNCTIONS
 
-//Different kinds of console logging
-const msg = 'this is a message ';
+// function funcName(arg1, arg2, arg3) {
+//     // execute some code
+// };
 
-//General log
-console.log(msg);
+// let x = sum(3,5);
 
-//Info Message
-console.info(msg);
+// function sum(a,b) {
+//     return a + b;
+// }
 
-//Warning message
-console.warn(msg);
+// //console.log(x);
 
-//Error message
-console.error(msg);
+// let welcome = function(fullName) {
+//     console.log(`hello ${fullName}`);
+// }
 
-//Make console log look unique with HTML/CSS
-console.log(`%c` + msg, `color: black; background-color: white; font-size 20px; padding: 5px;`);
+// //welcome();
 
-console.log(`%c` + msg + `%c` + msg, `color:red;`, `color:green;`);
+// let welcomeTwo = () => {
+//     console.log(`hello two`);
+// }
 
-const msgExercise = 'This is a message for the exercises! '
+// //welcomeTwo();
 
-//Exercise 1
-console.log(`Exercise 1`);
-console.log(`First Name: Richard`);
-console.log(`Second Name: Renshaw`);
-console.log(`From: Manchester`);
-console.log(`StarSign: Aries`);
+// welcomeThree = () => console.log(`hello three`);
 
-//Exercise 2
-console.log(`Exercise 2`);
-console.log(`%c` + msgExercise, `color: orange; font-weight: bold; font-family: fantasy; background-color: black; padding: 10px;`);
+// //welcomeThree();
 
+// welcomeName = fullName => console.log(`hello ${fullName}`);
 
+// welcomeName(`Rikk`);
 
+// 1. Create a function that takes in 2 parameters num1 and num2 and subtracts the two numbers.
+// 2. Create a function expression called welcome that take in name and age as a parameters. The outcome should be like so:
+// e.g. "My name is Cameron, i am 28 years old"
 
+// 3. Create an arrow function called powerUp that takes in two values n1 and n2. The arrow function will return the power of the two numbers.
+// e.g:
+// * n1=2, n2=3 will return 8 (2 x 2 x 2);
+// * n1=3, n2=3 will return 27 (3 x 3 x 3);
+// -> hint use Math.pow(
 
+subtraction = (num1, num2) => {
+    return num1 - num2;
+}
 
+console.log(subtraction(5,3))
 
+welcome = (name, age) => {
+    return `my name is ${name} & I am ${age} years old`
+}
 
+console.log(welcome("Rikk", 39))
 
+powerUp = (n1, n2) => {
+    return Math.pow(n1, n2);
+}
 
-
-
-
+console.log(powerUp(2,3))
+console.log(powerUp(3,3))

1d890285ede9aff6dc831c9f0aa92833fc3e5166
RikkRenner <Rikk62@gmail.com>
2021-09-29 15:25:09 +0100

    object arrays

--- javascript/script.js
+++ javascript/script.js
@@ -1,48 +1,85 @@
-// Console log
-console.log("Hello World!");
+// // Objects, Arrays & JSON
 
-//Different kinds of console logging
-const msg = 'this is a message ';
+// let animal = new Object();
+// animal["name"] = "Clifford";
+// animal["type"] = "Dog";
+// animal["size"] = "Big"
 
-//General log
-console.log(msg);
+// // console.log(animal)
+// // console.log(animal.name)
 
-//Info Message
-console.info(msg);
+// animal.colour = "Red"
 
-//Warning message
-console.warn(msg);
+// // console.log(animal);
 
-//Error message
-console.error(msg);
+// //Object Literal
+// let vehicle = {
+//     "type": "car",
+//     "seats": "5",
+//     "wheels": 4
+// }
 
-//Make console log look unique with HTML/CSS
-console.log(`%c` + msg, `color: black; background-color: white; font-size 20px; padding: 5px;`);
+// console.log(vehicle);
 
-console.log(`%c` + msg + `%c` + msg, `color:red;`, `color:green;`);
+// // OBJECTS can be stored in an ARRAY
+// let garage = [
+//     vehicle,
+//     {"type": "bike", "seats": 1, "wheels": 2}
+// ];
 
-const msgExercise = 'This is a message for the exercises! '
+// // console.log(garage)
 
-//Exercise 1
-console.log(`Exercise 1`);
-console.log(`First Name: Richard`);
-console.log(`Second Name: Renshaw`);
-console.log(`From: Manchester`);
-console.log(`StarSign: Aries`);
+// // lets loop through an ARRAY
 
-//Exercise 2
-console.log(`Exercise 2`);
-console.log(`%c` + msgExercise, `color: orange; font-weight: bold; font-family: fantasy; background-color: black; padding: 10px;`);
+// let x = [1,2,3,4,5];
+// let y = [15,2,-34,42,99,0]
 
+// for(let i = 0; i < x.length; i++){
+//     console.log(x[i])
+// }
 
+// // enhanced for loop
+// for(let i of y) {
+//     console.log(i)
+// }
 
+// // ARRAY OBJECT METHODS
+// // console.log(y.revere());
 
+// // console.log(y.join(`.-.`));
 
+// // JSON
 
+// let newJson = {
+//     "name": "Rikk",
+//     "age": 39
+// }
 
+// let MyName = JSON.parse(`{"name": "Rikk"}`);
+// console.log(myName.name);
 
+// let str = JSON.stringify(newJson);
+// console.log(str);
 
+// Exercise
+// 1. Create an array with the name myArray with 2 elements hello,everyone..
+// 2. Next print the length of the array
+// 3. Next use the push() method to add 3 elements to the array
+// 4. Next print the length of the array
+// 5. Next use shift() to remove an element
+// 6. Finally print the contents of the array using a for of loop.
 
+let myArray = ["hello", "world"]
+console.log(`length of array is ${myArray.length}`)
 
+myArray.push("hola", "bonjour", "kuday");
 
+console.log(`length of array is ${myArray.length}`)
 
+myArray.shift();
+
+console.log(`length of array is ${myArray.length}`)
+
+for(let greeting in myArray) {
+    console.log(myArray[greeting]);
+}

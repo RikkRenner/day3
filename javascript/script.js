d39533199a19b50d924daef91a26c3570cecb60f
RikkRenner <Rikk62@gmail.com>
2021-09-29 11:14:13 +0100

    add in total money for £4000

--- javascript/script.js
+++ javascript/script.js
@@ -54,8 +54,8 @@ let moneyPaidSoFar = 2348;
 let totalLeftToPay = totalMoney - moneyPaidSoFar;
 
 // Use interpolation for operations in string
-console.log(`The total bill is £4000 the remaining amount of money to be paid is £${totalMoney - moneyPaidSoFar}`);
+console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is £${totalMoney - moneyPaidSoFar}`);
 
 //Use variable and referance using interpolation
-console.log(`The total bill is £4000 the remaining amount of money to be paid is £${totalLeftToPay}`);
+console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
 
